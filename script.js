2439ef020d95f1dbce6494b4b44d088383d103dc
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2023-06-20 21:09:55 +0300

    ajout des toutes les contenues

---
+++ script.js
@@ -0,0 +1,9 @@
+function mOver(obj) {
+
+    document.getElementById('ani').style.transition = '5s';
+    document.getElementById('ani').style.display = "flex";
+}
+
+function mOut(obj) {
+    document.getElementById('ani').style.display = "none"
+}
